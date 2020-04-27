e6a493bdf0158b56aefd10ab42843f7cc4b1f348
peetsP <peetsm001@gmail.com>
2020-04-28 00:00:20 +0300

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,94 @@
+$(document).ready(function()
+{
+	"use strict";
+
+
+	var header = $('.header');
+	var headerSocial = $('.header_social');
+	var menu = $('.menu');
+	var menuActive = false;
+	var burger = $('.hamburger');
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+
+		setTimeout(function()
+		{
+			$(window).trigger('resize.px.parallax');
+		}, 375);
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+	initHomeSlider();
+	initIsotope();
+	initTestimonialsSlider();
+	initScrolling();
+	initInput();
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 127)
+		{
+			header.addClass('scrolled');
+			headerSocial.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+			headerSocial.removeClass('scrolled');
+		}
+	}
+
+	function initMenu()
+	{
+		if($('.menu').length)
+		{
+			var menu = $('.menu');
+			if($('.hamburger').length)
+			{
+				burger.on('click', function()
+				{
+					if(menuActive)
+					{
+						closeMenu();
+					}
+					else
+					{
+						openMenu();
+					}
+				});
+			}
+		}
+		if($('.menu_close').length)
+		{
+			var close = $('.menu_close');
+			close.on('click', function()
+			{
+				if(menuActive)
+				{
+					closeMenu();
+				}
+			});
+		}
+	}
+
+	function openMenu()
+	{
+		menu.addClass('active');
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		menu.removeClass('active');
+		menuActive = false;
+	}
+});
